9d8f7f3a28d655ff9a8563fcc71025d7f2d24db0
Hao Luo <howlowck@gmail.com>
2023-01-18 20:22:30 -0600

    fixed tf resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,15 +29,15 @@ data "azurerm_resource_group" "app" {
 
 resource "azurerm_service_plan" "app" {
   name                = "${local.app}-${var.environment}"
-  resource_group_name = azurerm_resource_group.app.name
-  location            = azurerm_resource_group.app.location
+  resource_group_name = data.azurerm_resource_group.app.name
+  location            = data.azurerm_resource_group.app.location
   os_type             = "Linux"
   sku_name            = "B2"
 }
 
 resource "azurerm_linux_web_app" "app" {
   name                = "${local.app}-${var.environment}"
-  resource_group_name = azurerm_resource_group.app.name
+  resource_group_name = data.azurerm_resource_group.app.name
   location            = azurerm_service_plan.app.location
   service_plan_id     = azurerm_service_plan.app.id
 
@@ -69,8 +69,8 @@ resource "azurerm_linux_web_app" "app" {
 
 resource "azurerm_storage_account" "app" {
   name                     = "sa${local.app}${var.environment}"
-  resource_group_name      = azurerm_resource_group.app.name
-  location                 = azurerm_resource_group.app.location
+  resource_group_name      = data.azurerm_resource_group.app.name
+  location                 = data.azurerm_resource_group.app.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -87,8 +87,8 @@ resource "azurerm_storage_table" "app_haodev" {
 
 resource "azurerm_key_vault" "app" {
   name                       = "kv${local.app}${var.environment}"
-  location                   = azurerm_resource_group.app.location
-  resource_group_name        = azurerm_resource_group.app.name
+  location                   = data.azurerm_resource_group.app.location
+  resource_group_name        = data.azurerm_resource_group.app.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
